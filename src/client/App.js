63799247a30cfb59318bce3153ec6f0048454d03
Jamal L <jamallyons8@gmail.com>
2021-02-24 19:44:22 -0500

    first push

---
+++ src/client/App.js
@@ -0,0 +1,13 @@
+import "../styles/App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<div className="border-2 border-black bg-blue-500">
+				<h1>Hello World</h1>
+			</div>
+		</div>
+	);
+}
+
+export default App;
