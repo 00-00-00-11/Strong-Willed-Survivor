edb4711495775caf2a3c9a538cb739e07ad4329e
Jamal L <jamallyons8@gmail.com>
2021-03-01 19:51:30 -0500

    yes?

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 export default function index() {
 	return (
 		<div>
-			<h1>Hello World! - coming from react!</h1>
+			<span>Hello World! - coming from react!</span>
 		</div>
 	);
 }
